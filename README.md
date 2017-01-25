fef0b06da9bc73fedfa091cef9a44b0572453167
Diego Camargo <dicamargov@gmail.com>
2017-01-25 14:05:27 -0600

    Update README.md and some variables

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # micro-shortener
 
 This is a micro in-memory url shortener
+
+## Usage
+
+```bash
+virtualenv venv
+source venv/bin/activate
+# ignore the lines above this one if you don't have virtualenv installed
+pip install -r requirements.txt
+python shortener.py
+
+# on another terminal
+curl -X POST -H "Content-Type: application/json" -d '{"url":"example.org"}' http://localhost:5000/api/shortcuts
+
+# on your browser open the following url, note that id is the one that you got from the past request
+http://localhost:5000/{id}
+```
+
--- shortener.py
+++ shortener.py
@@ -5,7 +5,7 @@ import random
 
 app = Flask(__name__)
 
-shortcuts = {}
+shortcuts_dict = {}
 
 def generate_id(size=7, chars=string.ascii_lowercase + string.digits):
   return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
@@ -17,21 +17,21 @@ def api_welcome():
 @app.route('/api/shortcuts', methods = ['GET'])
 def show_shortcuts():
   urls = []
-  for key in shortcuts.keys():
-    urls.append(shortcuts[key].to_dict())
+  for key in shortcuts_dict.keys():
+    urls.append(shortcuts_dict[key].to_dict())
   return jsonify(urls)
 
 @app.route('/api/shortcuts/<key>', methods = ['GET'])
 def show_shortcut(key):
-  if key in shortcuts:
-    return jsonify(shortcuts[key].to_dict())
+  if key in shortcuts_dict:
+    return jsonify(shortcuts_dict[key].to_dict())
   else:
     return page_not_found()
 
 @app.route('/<key>')
 def go_to(key):
-  if key in shortcuts:
-    shortcut = shortcuts[key]
+  if key in shortcuts_dict:
+    shortcut = shortcuts_dict[key]
     shortcut.use_once()
     return str.format('<meta http-equiv="refresh" content="0; url={}" />', shortcut.url)
   else:
@@ -39,23 +39,23 @@ def go_to(key):
 
 @app.route('/api/shortcuts', methods = ['POST'])
 def add_shortcut():
+  global shortcuts_dict
   if request.headers['Content-Type'] == 'application/json' and request.json['url'] != None:
-    global shortcuts
     dst = request.json['url']
     if not dst.startswith('http'):
       dst = 'http://' + dst
     shortcut = Shortcut(generate_id(), dst)
-    shortcuts[shortcut.id] = shortcut
+    shortcuts_dict[shortcut.id] = shortcut
     return jsonify(shortcut.to_dict())
   else:
     return "Unsupported Media Type ;)", 415
 
 @app.route('/api/shortcuts/<id>', methods = ['DELETE'])
 def remove_shortcut(id):
-  if id in shortcuts:
-    global shortcuts
-    shortcut = shortcuts[id]
-    shortcuts.pop(id)
+  global shortcuts_dict
+  if id in shortcuts_dict:
+    shortcut = shortcuts_dict[id]
+    shortcuts_dict.pop(id)
     return jsonify(shortcut.to_dict())
   else:
     return page_not_found()
